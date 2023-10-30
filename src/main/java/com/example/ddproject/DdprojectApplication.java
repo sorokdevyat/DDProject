0b62d665682739d9a7d332955aa50c8ea1eb2859
sorokdevyat49 <kissesksk@gmail.com>
2023-10-30 19:09:57 +0300

    spring boot first commit

---
+++ src/main/java/com/example/ddproject/DdprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ddproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ddproject/DdprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ddproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
